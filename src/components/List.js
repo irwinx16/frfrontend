d41b1ccad0523afcee2b43f470bae4950df64629
Irwin <irgranados16@gmail.com>
2020-12-20 17:26:50 -0600

    install axios and get data from api

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const List = () => {
+  const getList = async () => {
+    try {
+      const allList = await axios.get("https://cors-anywhere.herokuapp.com/https://fetch-hiring.s3.amazonaws.com/hiring.json");
+      console.log(allList.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-	return (
-		<div>
-			<p>My list to display</p>
-		</div>
-	)
-}
+  useEffect(() => {
+    getList();
+  });
+
+  return <div>Future list</div>;
+};
